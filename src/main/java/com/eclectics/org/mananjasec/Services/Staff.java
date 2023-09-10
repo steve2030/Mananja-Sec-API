c0f2749dfa11bedee9e849a73a9647dfa55f1168
steve2030 <stevemighty2030@gmail.com>
2023-09-10 12:48:47 +0300

    added An ArrayList

--- src/main/java/com/eclectics/org/mananjasec/Services/Staff.java
+++ src/main/java/com/eclectics/org/mananjasec/Services/Staff.java
@@ -3,9 +3,13 @@ package com.eclectics.org.mananjasec.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Service
 public class Staff {
     @Autowired
     StaffRepository staffRepository;
+    public List<Staff>staffs= Arrays.asList<>()
 
 }
